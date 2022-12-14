57cac22523f092a1d8d80e0e6da6dc9ab33f237d
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-12-15 05:14:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
+        fontFamily: "Gilroy"
       ),
       home: const Home(),
     );
