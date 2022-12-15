58e3eef3b098ad67706be882cfda0f9b0dc4a42e
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-12-16 02:38:29 +0530

    Widgets changed

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:seo/html/seo_widget.dart';
 
 class Texter extends StatelessWidget {
-  const Texter({Key? key, this.textStyle, this.text=""}) : super(key: key);
+  const Texter({Key? key, this.textStyle, required this.text}) : super(key: key);
 
   final TextStyle? textStyle;
   final String text;
@@ -19,19 +19,53 @@ class Texter extends StatelessWidget {
   }
 }
 
+class RichTexter extends StatelessWidget {
+  const RichTexter({Key? key, required this.child, required this.text}) : super(key: key);
+
+  final Widget child;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Seo.text(
+      text: text,
+      child: child,
+    );
+  }
+}
 
 class Imager extends StatelessWidget {
-  const Imager({Key? key, this.textStyle, this.path=""}) : super(key: key);
+  const Imager({Key? key, required this.altText, required this.path, this.colorBlendMode}) : super(key: key);
 
-  final TextStyle? textStyle;
-  final String path;
+  final String path, altText;
+  final BlendMode? colorBlendMode;
 
   @override
   Widget build(BuildContext context) {
     return Seo.image(
       src: "https://chrisbinsunny.github.io/$path",
+      alt: altText,
       child: Image.asset(
         path,
+          colorBlendMode: colorBlendMode
+      ),
+    );
+  }
+}
+
+class Linker extends StatelessWidget {
+  const Linker({Key? key, required this.name, required this.link, required this.child}) : super(key: key);
+
+  final String link, name;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Seo.link(
+      anchor: name,
+      href: link,
+      child: Image.asset(
+        link,
       ),
     );
   }
