ade92d094b939f7ee3f211177c5ada746e8a3521
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-12-17 05:09:57 +0530

    Update about.dart

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -14,6 +14,8 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,9 +23,9 @@ class _AboutState extends State<About> {
       //height: screenHeight(context)-70,  ///Reducing 70 for appbar
       color: const Color(0xff0c0c0c),
       //color: Colors.green,
-      constraints: const BoxConstraints(
+      constraints: BoxConstraints(
       minWidth: 500,
-        //minHeight: screenHeight(context)-70,
+        minHeight: screenHeight(context)-70,
       ),
       padding: EdgeInsets.symmetric(
         horizontal: screenWidth(context, mulBy: 0.05),
@@ -33,85 +35,88 @@ class _AboutState extends State<About> {
         direction: Axis.horizontal,
         crossAxisAlignment: WrapCrossAlignment.center,
         alignment: WrapAlignment.spaceEvenly,
+        runAlignment: WrapAlignment.spaceEvenly,
         spacing: 20,
+        runSpacing: 40,
         children: [
-          Container(
+          SizedBox(
             width: 600,
-            color: Colors.blueAccent.withOpacity(0.3),
-            height: screenHeight(context, mulBy: 0.86)-70,
-            alignment: Alignment.center,
-            child: SizedBox(
-              width: 600,
-              height: screenHeight(context, mulBy: 0.86)-70,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Texter(
-                    "About Chrisbin",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 34,
-                      color: Theme.of(context).secondaryHeaderColor
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Texter(
-                    "Programming was always my area of interest since teenage. My main objective is to make technology reach everyone, so "
-                      "I conduct tech talks at schools and colleges. Currently on a mission to make clean UI/UX using Flutter Web.",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 20,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            width: 600,
-            color: Colors.blueAccent.withOpacity(0.3),
-            height: screenHeight(context, mulBy: 0.86)-70,
-            alignment: Alignment.center,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Texter(
-                  "Skill Stack",
+                  "About Chrisbin",
                   style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 34,
-                      color: Theme.of(context).secondaryHeaderColor
+                    fontWeight: FontWeight.w600,
+                    fontSize: 34,
+                    color: Theme.of(context).secondaryHeaderColor
                   ),
                 ),
                 const SizedBox(
-                  height: 25,
+                  height: 30,
                 ),
-                Container(
-                  height: 450,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    color: const Color(0xff0c0c0c),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 0.7
-                    ),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Color(0x38ffffff),
-                        spreadRadius: 4,
-                        blurRadius: 15
-                      )
-                    ]
+                const Texter(
+                  "Programming was always my area of interest since teenage. My main objective is to make technology reach everyone, so "
+                    "I conduct tech talks at schools and colleges. Currently on a mission to make clean UI/UX using Flutter Web.",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 20,
                   ),
-                )
+                  textAlign: TextAlign.center,
+                ),
               ],
             ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Texter(
+                "Skill Stack",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 34,
+                    color: Theme.of(context).secondaryHeaderColor
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 480,
+                width: 400,
+                clipBehavior: Clip.antiAlias,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: const Color(0xff0c0c0c),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 0.7
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0x2dffffff),
+                      spreadRadius: 4,
+                      blurRadius: 15
+                    )
+                  ]
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: const Texter(
+                  "Flutter, Dart, Firebase, Supabase, Google Cloud Maps, GoLang",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 52,
+                      color: Color(0x6b3093c0)
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              )
+            ],
           ),
         ],
       ),
