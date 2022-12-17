eeb2e0f86f43fbd8f507f796d921ed9e6e3f3b0f
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-12-18 01:47:46 +0530

    Update projectView.dart

--- lib/widgets/projectView.dart
+++ lib/widgets/projectView.dart
@@ -6,9 +6,9 @@ import 'dart:ui' as ui;
 import 'package:portfolio/widgets/widgets.dart';
 
 class ProjectView extends StatefulWidget {
-  const ProjectView({Key? key, required this.image, required this.altText, required this.desc, required this.github, required this.link}) : super(key: key);
+  const ProjectView({Key? key, required this.name, required this.image, required this.altText, required this.desc, required this.github, required this.link}) : super(key: key);
 
-  final String image, altText, desc, github, link;
+  final String name, image, altText, desc, github, link;
 
   @override
   State<ProjectView> createState() => _ProjectViewState();
@@ -25,43 +25,75 @@ class _ProjectViewState extends State<ProjectView> {
 
       alignment: Alignment.center,
       child: Container(
-        height: screenHeight(context, mulBy: 0.55),
-        width: screenWidth(context, mulBy: 0.6),
+        // height: screenHeight(context, mulBy: 0.55),
+        // width: screenWidth(context, mulBy: 0.6),
+
         decoration: BoxDecoration(
             color: const Color(0xff111111),
             borderRadius: BorderRadius.circular(15),
             border: Border.all(color: Colors.white, width: 0.6)),
-        padding: EdgeInsets.symmetric(
+        constraints: BoxConstraints(
+          minHeight: screenHeight(context, mulBy: 0.55),
+        ),
+        padding: const EdgeInsets.symmetric(
           vertical: 30,
           horizontal: 30
         ),
-        child: Wrap(
-          direction: Axis.horizontal,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          alignment: WrapAlignment.spaceEvenly,
-          runAlignment: WrapAlignment.spaceEvenly,
-          spacing: 20,
-          runSpacing: 40,
-          children: [
-            Container(
-              width: screenWidth(context, mulBy: 0.25),
-              height: screenHeight(context, mulBy: 0.4),
-              decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(color: Colors.white, width: 0.6)),
-              child: Imager(
-                altText: widget.altText,
-                path: widget.image,
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(color: Colors.white, width: 0.6)),
-            ),
-          ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Wrap(
+              direction: Axis.horizontal,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              alignment: WrapAlignment.spaceEvenly,
+              runAlignment: WrapAlignment.spaceEvenly,
+              spacing: 20,
+              runSpacing: 40,
+              children: [
+                Column(
+                  children: [
+                    Texter(
+                      widget.name,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 34,
+                          color: Theme.of(context).secondaryHeaderColor),
+                    ),
+                    Container(
+                      width: screenWidth(context, mulBy: 0.3),
+                      constraints: const BoxConstraints(
+                        maxHeight: 250,
+                        minWidth: 400,
+                      ),
+                      child: Texter(
+                        widget.desc,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Container(
+                  width: screenWidth(context, mulBy: 0.35),
+                  constraints: const BoxConstraints(
+                      minWidth: 500
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.white, width: 0.6)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Imager(
+                      altText: widget.altText,
+                      path: widget.image,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
