1729fd0cb9d966f85adb7a5ffda7f7712341a490
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-12-24 02:31:40 +0530

    Update drawer.dart

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart';
 
 class CustomEndDrawer extends StatelessWidget {
   const CustomEndDrawer({Key? key}) : super(key: key);
@@ -9,86 +13,91 @@ class CustomEndDrawer extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
       if(constraints.biggest.width<550){
-        return Drawer(
-          backgroundColor: Colors.black,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [const
-              DrawerHeader(
-                child: Center(
-                  child: Text(
-                  'DREAM',
-              ),
-                ),
+        return ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20, tileMode: TileMode.clamp),
+            child: Drawer(
+              backgroundColor: Colors.white.withOpacity(0.07),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DrawerHeader(
+                    child: Center(
+                      child: Texter(
+                      'CHRISBIN',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 35,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 5,
+                        ),
+                  ),
+                    ),
 
-              ),
-              ListTile(
-                title: const Text(
-                    "Color Finder",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18
-                ),
-                ),
-                onTap: cfOnTap(context),
-                hoverColor:Colors.deepPurpleAccent.withOpacity(0.1),
-                focusColor: Colors.deepPurpleAccent.withOpacity(0.3),
-                enabled: true,
-                selectedTileColor: Colors.deepPurpleAccent.withOpacity(0.3),
-                selected: ((ModalRoute.of(context)!.settings.name=="/color-finder/")||
-                    (ModalRoute.of(context)!.settings.name=="/color-finder")||
-                    (ModalRoute.of(context)!.settings.name=="/")
-                ),
-                trailing: const Icon(
-                  Icons.navigate_next,
-                  color: Colors.white,
-                ),
-              ),
-              ListTile(
-                title: const Text(
-                  "Gradient Maker",
-                  style: TextStyle(
+                  ),
+                  ListTile(
+                    title: const Texter(
+                        "Color Finder",
+                    style: TextStyle(
                       color: Colors.white,
                       fontSize: 18
-                  ),
-                ),
-                onTap: gmOnTap(context),
-                hoverColor: Colors.deepPurpleAccent.withOpacity(0.1),
-                focusColor: Colors.deepPurpleAccent.withOpacity(0.3),
-                enabled: true,
-                selectedTileColor: Colors.deepPurpleAccent.withOpacity(0.3),
-                selected: ((ModalRoute.of(context)!.settings.name=="/gradient-maker/")||
-        (ModalRoute.of(context)!.settings.name=="/gradient-maker")
-        ),
-                trailing: const Icon(
-                  Icons.navigate_next,
-                  color: Colors.white,
-                ),
-              ),
-              const Spacer(),
-              Container(
-            height: 50,
-                width: 50,
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurpleAccent.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                child: IconButton(
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  icon: const Icon(
-                    CupertinoIcons.xmark,
+                    ),
+                    ),
+                    onTap: cfOnTap(context),
+                    hoverColor: Theme.of(context).secondaryHeaderColor.withOpacity(0.1),
+                    focusColor: Theme.of(context).secondaryHeaderColor.withOpacity(0.3),
+                    enabled: true,
+                    selectedTileColor: Theme.of(context).secondaryHeaderColor.withOpacity(0.4),
+                    selected: true,
+                    leading: const Icon(
+                    Icons.navigate_before,
                     color: Colors.white,
-                  ), onPressed: () {
-                    Scaffold.of(context).closeDrawer();
-                },
-                ),
+                  ),
+                  ),
+                  ListTile(
+                    title: const Texter(
+                      "Color Finder",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18
+                      ),
+                    ),
+                    onTap: cfOnTap(context),
+                    hoverColor:Colors.deepPurpleAccent.withOpacity(0.1),
+                    focusColor: Colors.deepPurpleAccent.withOpacity(0.3),
+                    enabled: true,
+                    selectedTileColor: Colors.deepPurpleAccent.withOpacity(0.3),
+                    leading: const Icon(
+                      Icons.navigate_before,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                height: 50,
+                    width: 50,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 20
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).secondaryHeaderColor.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: IconButton(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      icon: const Icon(
+                        CupertinoIcons.xmark,
+                        color: Colors.white,
+                      ), onPressed: () {
+                        Scaffold.of(context).closeEndDrawer();
+                    },
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         );
       }
