f5b1a9154ece4ef827feb8365f8c1433d5c5aa31
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-06 00:35:55 +0530

    Update drawer.dart

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 import 'dart:ui';
 
@@ -18,10 +19,7 @@ class CustomEndDrawer extends StatefulWidget {
 
 class _CustomEndDrawerState extends State<CustomEndDrawer> {
 
-  final hovered=ValueNotifier<int>(-1);
-  final viewing=ValueNotifier<int>(0);
-
-
+  final hovered=ValueNotifier<int>(-1), viewing=ValueNotifier<int>(0);
   @override
   void initState() {
     super.initState();
@@ -47,99 +45,115 @@ class _CustomEndDrawerState extends State<CustomEndDrawer> {
                   width: 0.5
                 )
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  DrawerHeader(
-                    child: Center(
-                      child: Texter(
-                      'CHRISBIN',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 35,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 5,
+              child: ValueListenableBuilder(
+                valueListenable: viewing,
+                builder: (context, value, child) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      DrawerHeader(
+                        child: Center(
+                          child: Texter(
+                            'CHRISBIN',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 35,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 5,
+                            ),
+                          ),
                         ),
-                  ),
-                    ),
 
-                  ),
-                  AppBarButton(
-                      onTap: (){
-                        widget.scrollController.position
-                            .ensureVisible(
-                            widget.keys[0].currentContext!.findRenderObject()!,
-                            curve: Curves.easeInOut,
+                      ),
+                      AppBarButton(
+                        viewing: value,
+                        count: 0,
+                        onTap: (){
+                          widget.scrollController.position
+                              .ensureVisible(
+                              widget.keys[0].currentContext!.findRenderObject()!,
+                              curve: Curves.easeInOut,
 
-                            duration: const Duration(seconds: 1),
-                            alignment: -0.7
-                        );
-                      },
-                      text: 'Home',
-                  ),
-                  AppBarButton(
-                      onTap: (){
-                        widget.scrollController.position
-                            .ensureVisible(
-                            widget.keys[1].currentContext!.findRenderObject()!,
-                            curve: Curves.easeInOut,
+                              duration: const Duration(seconds: 1),
+                              alignment: -0.7
+                          );
+                        },
+                        text: 'Home',
+                      ),
+                      AppBarButton(
+                        viewing: value,
+                        count: 1,
 
-                            duration: const Duration(seconds: 1),
-                            alignment: -0.7
-                        );
-                      },
-                      text: 'About',
-                  ),
-                  AppBarButton(
-                      onTap: (){
-                        widget.scrollController.position
-                            .ensureVisible(
-                            widget.keys[2].currentContext!.findRenderObject()!,
-                            curve: Curves.easeInOut,
+                        onTap: (){
+                          widget.scrollController.position
+                              .ensureVisible(
+                              widget.keys[1].currentContext!.findRenderObject()!,
+                              curve: Curves.easeInOut,
 
-                            duration: const Duration(seconds: 1),
-                            alignment: -0.7
-                        );
-                      },
-                      text: 'Projects',
-                  ),
-                  AppBarButton(
-                      onTap: (){
-                        widget.scrollController.position
-                            .ensureVisible(
-                            widget.keys[2].currentContext!.findRenderObject()!,
-                            curve: Curves.easeInOut,
+                              duration: const Duration(seconds: 1),
+                              alignment: -0.7
+                          );
+                        },
+                        text: 'About',
+                      ),
+                      AppBarButton(
+                        viewing: value,
+                        count: 2,
 
-                            duration: const Duration(seconds: 1),
-                            alignment: -0.7
-                        );
-                      },
-                      text: 'Social',
-                  ),
-                  const Spacer(),
-                  Container(
-                height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 20
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).secondaryHeaderColor.withOpacity(0.4),
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: IconButton(
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      icon: const Icon(
-                        CupertinoIcons.xmark,
-                        color: Colors.white,
-                      ), onPressed: () {
-                        Scaffold.of(context).closeEndDrawer();
-                    },
-                    ),
-                  ),
-                ],
+                        onTap: (){
+                          widget.scrollController.position
+                              .ensureVisible(
+                              widget.keys[2].currentContext!.findRenderObject()!,
+                              curve: Curves.easeInOut,
+
+                              duration: const Duration(seconds: 1),
+                              alignment: -0.7
+                          );
+                        },
+                        text: 'Projects',
+                      ),
+                      AppBarButton(
+                        viewing: value,
+                        count: 3,
+
+                        onTap: (){
+                          widget.scrollController.position
+                              .ensureVisible(
+                              widget.keys[2].currentContext!.findRenderObject()!,
+                              curve: Curves.easeInOut,
+
+                              duration: const Duration(seconds: 1),
+                              alignment: -0.7
+                          );
+                        },
+                        text: 'Social',
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 20
+                        ),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).secondaryHeaderColor.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                        child: IconButton(
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          icon: const Icon(
+                            CupertinoIcons.xmark,
+                            color: Colors.white,
+                          ), onPressed: () {
+                          Scaffold.of(context).closeEndDrawer();
+                        },
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
           ),
@@ -148,55 +162,24 @@ class _CustomEndDrawerState extends State<CustomEndDrawer> {
       if(
       Scaffold.of(context).isEndDrawerOpen
       ){
-        Navigator.pop(context);
+        Timer(Duration.zero, () {
+          Scaffold.of(context).closeEndDrawer();
+
+        },);
       }
       return const Nothing();
     },
     );
   }
-
-  VoidCallback? gmOnTap(BuildContext context){
-    switch(ModalRoute.of(context)!.settings.name){
-      case "/gradient-maker":
-      case "/gradient-maker/":
-        return null;
-        break;
-      case "/color-finder":
-      case "/color-finder/":
-      case "/":
-        return (){
-          Navigator.of(context).pushNamed("/gradient-maker");
-        };
-        break;
-    }
-    return null;
-  }
-
-  VoidCallback? cfOnTap(BuildContext context){
-    switch(ModalRoute.of(context)!.settings.name){
-      case "/gradient-maker":
-      case "/gradient-maker/":
-        return (){
-          Navigator.of(context).pushNamed("/color-finder");
-        };
-        break;
-      case "/color-finder":
-      case "/color-finder/":
-      case "/":
-        return null;
-
-        break;
-    }
-    return null;
-  }
 }
 
 
 class AppBarButton extends StatelessWidget {
-  const AppBarButton({Key? key, required this.onTap, required this.text,}) : super(key: key);
+  const AppBarButton({Key? key, required this.onTap, required this.text, required this.viewing, required this.count}) : super(key: key);
 
   final VoidCallback onTap;
   final String text;
+  final int count, viewing;
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -212,6 +195,7 @@ class AppBarButton extends StatelessWidget {
       focusColor: Theme.of(context).secondaryHeaderColor.withOpacity(0.3),
       enabled: true,
       selectedTileColor: Theme.of(context).secondaryHeaderColor.withOpacity(0.4),
+      selected: viewing==count,
       leading: const Icon(
         Icons.navigate_before,
         color: Colors.white,
