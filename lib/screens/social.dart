e2f712a60408eb2abef95a2fe678732d94cd32b3
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-06 02:02:44 +0530

    Update social.dart

--- lib/screens/social.dart
+++ lib/screens/social.dart
@@ -21,131 +21,59 @@ class _SocialState extends State<Social> {
 
     return Container(
       width: screenWidth(context, mulBy: 1),
-      height: screenHeight(context),  ///Reducing 70 for appbar
+      height: screenHeight(context),
       color: const Color(0xff0c0c0c),
       constraints:const BoxConstraints(
       minWidth: 500,
-        minHeight:  725,  ///Reducing 70 for appbar
+        minHeight:  725,
       ),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return Stack(
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        runAlignment: WrapAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Seo.text(
-                text: "App Developer | Engineer | Designer | Speaker",
-                child: Text(
-                  "Flutter Dart Firebase Supabase Google Cloud Maps GoLang "*10,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 80,
-                      color: Color(0x83093c0)
-                  ),
-                  textAlign: TextAlign.justify,
+              SizedBox(
+                height: screenHeight(context, mulBy: 0.3),
+                child: Imager(
+                  altText: "QR code to get Chrisbin's contact vCard",
+                  path:"images/qrCode.png",
+                  imageFit: BoxFit.scaleDown,
                 ),
               ),
-              Container(
-                width: constraints.maxWidth,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10
+              SizedBox(
+                height: 10,
+              ),
+              Texter("  Chrisbin Sunny",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: screenHeight(context, mulBy: 0.08),
-                    ),
-                    const SizedBox(
-                      height: 250,
-                      child: Imager(
-                        path: "images/macGuy.png",
-                        altText: "Memoji using macbook. WWDC",
-                        colorBlendMode: BlendMode.colorDodge,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Texter("   App Developer | Speaker",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w200
+                ),
+              ),
+              TextButton(
+                  onPressed: (){},
+                  child: Texter(
+                      "(IND) +91 83300 70512",
 
-                      child: RichTexter(
-                        text: "Chrisbin Sunny",
-                        child: RichText(
-                          text: const TextSpan(
-                            text: "Chrisbin\n",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800,
-                                color: Color(0xff5dc8f8),
-                                fontSize: 70,
-                              fontFamily: "Gilroy"
-                            ),
-                            children: [
-                              TextSpan(
-                                  text: "Sunny",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    color: Color(0xff065a9d),
-                                    fontSize: 70,
-                                    height: 0.7
-                                ),
-                              ),
-                            ]
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenHeight(context, mulBy: 0.03),
-                    ),
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: screenHeight(context, mulBy: 0.25),
-                        width: 400,
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.w100,
-                            color: Colors.white.withOpacity(0.9),
-                            shadows: const [
-                              BoxShadow(
-                                blurRadius: 17.0,
-                                spreadRadius: 20,
-                                color: Color(0xff5dc8f8),
-                              ),
-                            ],
-                          ),
-                          child: AnimatedTextKit(
-                            repeatForever: true,
-                            pause: const Duration(seconds: 1),
-                            animatedTexts: [
-                              FlickerAnimatedText(
-                                'App Developer',
-                               ),
-                              FlickerAnimatedText(
-                                'Speaker',
-                              ),
-                              FlickerAnimatedText(
-                                'Engineer',
-                              ),
-                              FlickerAnimatedText(
-                                'Designer',
-                              ),
-                            ],
-                            displayFullTextOnTap: true,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                style: TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w200
+    ),
               )
             ],
-          );
-        },
+          ),
+        ],
       ),
     );
   }
