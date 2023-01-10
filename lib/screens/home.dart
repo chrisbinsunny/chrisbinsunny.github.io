45beb494147bdcd4709dd3d1da22ebb2f8f75dcd
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-11 00:07:20 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@
 
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:portfolio/components/contactCard.dart';
 import 'package:portfolio/sizes.dart';
 import 'package:portfolio/widgets/widgets.dart';
 import 'package:seo/html/seo_widget.dart';
@@ -17,131 +16,136 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+
+
+
     return Container(
       width: screenWidth(context, mulBy: 1),
-      //height: screenHeight(context),  ///Reducing 70 for appbar
-      constraints: BoxConstraints(
+      height: screenHeight(context),  ///Reducing 70 for appbar
+      color: const Color(0xff0c0c0c),
+      constraints:const BoxConstraints(
       minWidth: 500,
-        minHeight:  screenHeight(context),  ///Reducing 70 for appbar
+        minHeight:  725,  ///Reducing 70 for appbar
       ),
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Color(0xff0c0c0c),
-                Color(0xff0f1617),
-                Color(0xff0c0c0c),
-
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter
-          )
-      ),
-      padding: const EdgeInsets.symmetric(
-          horizontal: 10
-      ),
-      child: Wrap(
-        alignment: WrapAlignment.spaceEvenly,
-        runAlignment: WrapAlignment.spaceEvenly,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Stack(
             children: [
-              SizedBox(
-                height: screenHeight(context, mulBy: 0.08),
-              ),
-              const SizedBox(
-                height: 250,
-                child: Imager(
-                  path: "images/macGuy.png",
-                  altText: "Memoji using macbook. WWDC",
-                  colorBlendMode: BlendMode.colorDodge,
+              Seo.text(
+                text: "App Developer | Engineer | Designer | Speaker",
+                child: Text(
+                  "Flutter Dart Firebase Supabase Google Cloud Maps GoLang "*10,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 80,
+                      color: Color(0x53093c0)
+                  ),
+                  textAlign: TextAlign.justify,
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              FittedBox(
-                fit: BoxFit.scaleDown,
+              Container(
+                width: constraints.maxWidth,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: screenHeight(context, mulBy: 0.08),
+                    ),
+                    const SizedBox(
+                      height: 250,
+                      child: Imager(
+                        path: "images/macGuy.png",
+                        altText: "Memoji using macbook. WWDC",
+                        colorBlendMode: BlendMode.colorDodge,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
 
-                child: RichTexter(
-                  text: "Chrisbin Sunny",
-
-                  child: RichText(
-
-                    text: const TextSpan(
-                        text: "Chrisbin\n",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w800,
-                            color: Color(0xff5dc8f8),
-                            fontSize: 70,
-                            fontFamily: "Gilroy"
-                        ),
-                        children: [
-                          TextSpan(
-                            text: "Sunny",
+                      child: RichTexter(
+                        text: "Chrisbin Sunny",
+                        child: RichText(
+                          text: const TextSpan(
+                            text: "Chrisbin\n",
                             style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                color: Color(0xff065a9d),
+                                fontWeight: FontWeight.w800,
+                                color: Color(0xff5dc8f8),
                                 fontSize: 70,
-                                height: 0.7
+                              fontFamily: "Gilroy"
                             ),
+                            children: [
+                              TextSpan(
+                                  text: "Sunny",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xff065a9d),
+                                    fontSize: 70,
+                                    height: 0.7
+                                ),
+                              ),
+                            ]
                           ),
-                        ]
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ),
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: screenHeight(context, mulBy: 0.03),
-              ),
-              FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Container(
-                  alignment: Alignment.center,
-                  height: screenHeight(context, mulBy: 0.25),
-                  width: 400,
-                  child: DefaultTextStyle(
-                    style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.w100,
-                      color: Colors.white.withOpacity(0.9),
-                      shadows: const [
-                        BoxShadow(
-                          blurRadius: 17.0,
-                          spreadRadius: 20,
-                          color: Color(0xff5dc8f8),
-                        ),
-                      ],
+                    SizedBox(
+                      height: screenHeight(context, mulBy: 0.03),
                     ),
-                    child: AnimatedTextKit(
-                      repeatForever: true,
-                      pause: const Duration(seconds: 1),
-                      animatedTexts: [
-                        FlickerAnimatedText(
-                          'App Developer',
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: screenHeight(context, mulBy: 0.25),
+                        width: 400,
+                        child: DefaultTextStyle(
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.w100,
+                            color: Colors.white.withOpacity(0.9),
+                            shadows: const [
+                              BoxShadow(
+                                blurRadius: 17.0,
+                                spreadRadius: 20,
+                                color: Color(0xff5dc8f8),
+                              ),
+                            ],
+                          ),
+                          child: AnimatedTextKit(
+                            repeatForever: true,
+                            pause: const Duration(seconds: 1),
+                            animatedTexts: [
+                              FlickerAnimatedText(
+                                'App Developer',
+                               ),
+                              FlickerAnimatedText(
+                                'Speaker',
+                              ),
+                              FlickerAnimatedText(
+                                'Engineer',
+                              ),
+                              FlickerAnimatedText(
+                                'Designer',
+                              ),
+                            ],
+                            displayFullTextOnTap: true,
+                          ),
                         ),
-                        FlickerAnimatedText(
-                          'Speaker',
-                        ),
-                        FlickerAnimatedText(
-                          'Engineer',
-                        ),
-                        FlickerAnimatedText(
-                          'Designer',
-                        ),
-                      ],
-                      displayFullTextOnTap: true,
-                    ),
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
-          ),
-          ContactCard()
-        ],
+          );
+        },
       ),
     );
   }
