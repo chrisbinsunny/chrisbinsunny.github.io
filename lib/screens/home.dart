4389478c2eca38787a10c0f903bceca21299788a
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-10 23:46:29 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,122 +20,128 @@ class _HomeState extends State<Home> {
     return Container(
       width: screenWidth(context, mulBy: 1),
       //height: screenHeight(context),  ///Reducing 70 for appbar
-      color: const Color(0xff0c0c0c),
       constraints: BoxConstraints(
       minWidth: 500,
         minHeight:  screenHeight(context),  ///Reducing 70 for appbar
       ),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return Container(
-            width: constraints.maxWidth,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10
-            ),
-            child: Wrap(
-              alignment: WrapAlignment.spaceEvenly,
-              runAlignment: WrapAlignment.spaceEvenly,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: screenHeight(context, mulBy: 0.08),
-                    ),
-                    const SizedBox(
-                      height: 250,
-                      child: Imager(
-                        path: "images/macGuy.png",
-                        altText: "Memoji using macbook. WWDC",
-                        colorBlendMode: BlendMode.colorDodge,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color(0xff0c0c0c),
+                Color(0xff0f1617),
+                Color(0xff0c0c0c),
 
-                      child: RichTexter(
-                        text: "Chrisbin Sunny",
-                        child: RichText(
-                          text: const TextSpan(
-                            text: "Chrisbin\n",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800,
-                                color: Color(0xff5dc8f8),
-                                fontSize: 70,
-                              fontFamily: "Gilroy"
-                            ),
-                            children: [
-                              TextSpan(
-                                  text: "Sunny",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    color: Color(0xff065a9d),
-                                    fontSize: 70,
-                                    height: 0.7
-                                ),
-                              ),
-                            ]
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenHeight(context, mulBy: 0.03),
-                    ),
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: screenHeight(context, mulBy: 0.25),
-                        width: 400,
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.w100,
-                            color: Colors.white.withOpacity(0.9),
-                            shadows: const [
-                              BoxShadow(
-                                blurRadius: 17.0,
-                                spreadRadius: 20,
-                                color: Color(0xff5dc8f8),
-                              ),
-                            ],
-                          ),
-                          child: AnimatedTextKit(
-                            repeatForever: true,
-                            pause: const Duration(seconds: 1),
-                            animatedTexts: [
-                              FlickerAnimatedText(
-                                'App Developer',
-                               ),
-                              FlickerAnimatedText(
-                                'Speaker',
-                              ),
-                              FlickerAnimatedText(
-                                'Engineer',
-                              ),
-                              FlickerAnimatedText(
-                                'Designer',
-                              ),
-                            ],
-                            displayFullTextOnTap: true,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                ContactCard()
               ],
-            ),
-          );
-        },
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter
+          )
+      ),
+      padding: const EdgeInsets.symmetric(
+          horizontal: 10
+      ),
+      child: Wrap(
+        alignment: WrapAlignment.spaceEvenly,
+        runAlignment: WrapAlignment.spaceEvenly,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: screenHeight(context, mulBy: 0.08),
+              ),
+              const SizedBox(
+                height: 250,
+                child: Imager(
+                  path: "images/macGuy.png",
+                  altText: "Memoji using macbook. WWDC",
+                  colorBlendMode: BlendMode.colorDodge,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              FittedBox(
+                fit: BoxFit.scaleDown,
+
+                child: RichTexter(
+                  text: "Chrisbin Sunny",
+
+                  child: RichText(
+
+                    text: const TextSpan(
+                        text: "Chrisbin\n",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            color: Color(0xff5dc8f8),
+                            fontSize: 70,
+                            fontFamily: "Gilroy"
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Sunny",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xff065a9d),
+                                fontSize: 70,
+                                height: 0.7
+                            ),
+                          ),
+                        ]
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight(context, mulBy: 0.03),
+              ),
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Container(
+                  alignment: Alignment.center,
+                  height: screenHeight(context, mulBy: 0.25),
+                  width: 400,
+                  child: DefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white.withOpacity(0.9),
+                      shadows: const [
+                        BoxShadow(
+                          blurRadius: 17.0,
+                          spreadRadius: 20,
+                          color: Color(0xff5dc8f8),
+                        ),
+                      ],
+                    ),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      pause: const Duration(seconds: 1),
+                      animatedTexts: [
+                        FlickerAnimatedText(
+                          'App Developer',
+                        ),
+                        FlickerAnimatedText(
+                          'Speaker',
+                        ),
+                        FlickerAnimatedText(
+                          'Engineer',
+                        ),
+                        FlickerAnimatedText(
+                          'Designer',
+                        ),
+                      ],
+                      displayFullTextOnTap: true,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          ContactCard()
+        ],
       ),
     );
   }
