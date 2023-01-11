18545ba95565ec0b537bfb4f2825204cf089e6f2
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-11 18:28:46 +0530

    Update variables.dart

--- lib/constants/variables.dart
+++ lib/constants/variables.dart
@@ -1,5 +1,15 @@
 
+import 'package:flutter/material.dart';
+
 class Variables{
+
+  static List<GlobalKey> keys=[
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+  ];
+  static ScrollController scrollController= ScrollController();
 
 
 
